944ac88939dfc8c192a34c88857f2ce8c37c9960
Brandon Lam <whoisbrandonlam@gmail.com>
2020-06-10 15:01:00 -0400

    Increase glide cache size

--- app/src/main/java/com/b_lam/resplash/ResplashGlideModule.kt
+++ app/src/main/java/com/b_lam/resplash/ResplashGlideModule.kt
@@ -1,7 +1,16 @@
 package com.b_lam.resplash
 
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.cache.LruResourceCache
 import com.bumptech.glide.module.AppGlideModule
 
 @GlideModule
-class ResplashGlideModule : AppGlideModule()
+class ResplashGlideModule : AppGlideModule() {
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        val memoryCacheSizeBytes: Long = 1024 * 1024 * 500 // 500 MB
+        builder.setMemoryCache(LruResourceCache(memoryCacheSizeBytes))
+    }
+}
