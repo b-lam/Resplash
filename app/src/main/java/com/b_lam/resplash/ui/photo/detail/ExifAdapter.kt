a2fc357ac1a2519a2a530d365caa66064488aa5a
Brandon Lam <whoisbrandonlam@gmail.com>
2020-06-13 15:58:25 -0400

    Add camera make to displayed exif data

--- app/src/main/java/com/b_lam/resplash/ui/photo/detail/ExifAdapter.kt
+++ app/src/main/java/com/b_lam/resplash/ui/photo/detail/ExifAdapter.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.b_lam.resplash.R
 import com.b_lam.resplash.data.photo.model.Photo
+import java.util.*
 
 class ExifAdapter(
     val context: Context
@@ -28,7 +29,7 @@ class ExifAdapter(
         val pairs = mutableListOf<Pair<Int, SpannableStringBuilder>>()
         val unknown = SpannableStringBuilder(context.getString(R.string.unknown))
         photo.exif?.let {
-            pairs.add(R.string.camera to if (it.model != null) SpannableStringBuilder().append(it.model) else unknown)
+            pairs.add(R.string.camera to if (it.model != null) SpannableStringBuilder().append(formCameraName(it.make, it.model)) else unknown)
             pairs.add(R.string.aperture to if (it.aperture != null) SpannableStringBuilder().italic { append("f") }.append("/${it.aperture}") else unknown)
             pairs.add(R.string.focal_length to if (it.focal_length != null) SpannableStringBuilder("${it.focal_length}mm") else unknown)
             pairs.add(R.string.shutter_speed to if (it.exposure_time != null) SpannableStringBuilder("${it.exposure_time}s") else unknown)
@@ -36,6 +37,18 @@ class ExifAdapter(
             pairs.add(R.string.dimensions to if (photo.width != null && photo.height != null) SpannableStringBuilder("${photo.width} × ${photo.height}") else unknown)
         }
         submitList(pairs)
+    }
+
+    private fun formCameraName(make: String?, model: String): String {
+        val makeList = make?.split(" ")?.map { it.trim() }
+        val modelList = model.split(" ").map { it.trim() }
+        return if (makeList?.map { it.toLowerCase(Locale.ROOT) }
+                ?.intersect(modelList.map { it.toLowerCase(Locale.ROOT) })
+                ?.isEmpty() == true) {
+            "${makeList.first()} $model"
+        } else {
+            model
+        }
     }
 
     companion object {
