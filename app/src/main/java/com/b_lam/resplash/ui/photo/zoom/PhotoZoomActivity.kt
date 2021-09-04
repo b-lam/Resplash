82dcddef2f4f9fdb7a8949f5bbfe5e18936a8484
Lars <29163322+Drumber@users.noreply.github.com>
2021-09-04 16:24:51 +0200

    Added system UI toggle on click in PhotoZoomActivity

--- app/src/main/java/com/b_lam/resplash/ui/photo/zoom/PhotoZoomActivity.kt
+++ app/src/main/java/com/b_lam/resplash/ui/photo/zoom/PhotoZoomActivity.kt
@@ -3,8 +3,9 @@ package com.b_lam.resplash.ui.photo.zoom
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.ViewModel
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.b_lam.resplash.R
@@ -19,31 +20,51 @@ class PhotoZoomActivity : BaseActivity(R.layout.activity_photo_zoom) {
 
     override val binding: ActivityPhotoZoomBinding by viewBinding()
 
+    private var isSystemUiVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        toggleSystemUI(false)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-            window.insetsController?.let {
-                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            window.decorView.setOnApplyWindowInsetsListener { view, windowInsets ->
+                val suppliedInsets = view.onApplyWindowInsets(windowInsets)
+                isSystemUiVisible = suppliedInsets.isVisible(
+                    WindowInsetsCompat.Type.statusBars()
+                            or WindowInsetsCompat.Type.navigationBars()
+                )
+                suppliedInsets
             }
         } else {
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            window.decorView.setOnSystemUiVisibilityChangeListener {
+                isSystemUiVisible = (it and View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0
+            }
+        }
+
+        binding.zoomImageView.setOnClickListener {
+            toggleSystemUI(!isSystemUiVisible)
         }
 
         val url = intent.getStringExtra(EXTRA_PHOTO_URL)
 
         url?.let {
             binding.zoomImageView.loadPhotoUrl(it)
-        }  ?: run {
+        } ?: run {
             toast(R.string.oops)
             finish()
+        }
+    }
+
+    private fun toggleSystemUI(showSystemUI: Boolean) {
+        WindowInsetsControllerCompat(window, binding.root).let {
+            it.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            if (showSystemUI) {
+                it.show(WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
+            } else {
+                it.hide(WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
+            }
         }
     }
 
